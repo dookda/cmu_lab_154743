8de58a5366ea3d89bb6d1324a3cab6090893fddd
sakda homhuan <sakda.homhuan@gmail.com>
2023-07-21 11:44:36 +0700

    update

---
+++ lab_api.py
@@ -0,0 +1,78 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+tasks = [
+    {
+        'id': 1,
+        'title': 'Buy groceries',
+        'description': 'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': 'Learn Python',
+        'description': 'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+
+@app.route('/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+
+@app.route('/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+
+    if not request.json:
+        abort(400)
+
+    if 'title' in request.json and type(request.json['title']) != str:
+        abort(400)
+
+    if 'description' in request.json and type(request.json['description']) is not str:
+        abort(400)
+
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+
+    task[0]['description'] = request.json.get(
+        'description', task[0]['description'])
+
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+
+    return jsonify({'task': task[0]})
+
+
+if __name__ == '__main__':
+    app.run(port=4000, debug=True)
