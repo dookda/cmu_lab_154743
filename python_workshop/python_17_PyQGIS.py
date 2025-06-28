d29daa114ad8a1e910c77ec0ed56ccb7beff2c0b
dookda <sakda.homhuan@gmail.com>
2025-06-28 11:59:06 +0700

    update

--- python_workshop/python_17_PyQGIS.py
+++ python_workshop/python_17_PyQGIS.py
@@ -3,7 +3,7 @@ from qgis.core import *
 import glob
 
 # vill = "/Users/sakdahomhuan/Dev/workspace/cm_dwr/cm_dwr_village_4326.shp"
-#iface.addVectorLayer(vill, "cm village", 'ogr')
+# iface.addVectorLayer(vill, "cm village", 'ogr')
 
 dir = os.chdir("/Users/sakdahomhuan/Dev/workspace/cm_dwr")
 shp = glob.glob("*.shp")
@@ -12,4 +12,3 @@ for s in shp:
     print(s.split('.'))
     sname = s.split('.')[0]
     iface.addVectorLayer(s, sname, 'ogr')
-    
